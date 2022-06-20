364adef27f37fdb3665efb5f138209e23885e6c5
Zachary Chiodini <zacharybchiodini@gmail.com>
2022-06-20 14:31:10 -0400

    classes for cards

--- cards.py
+++ cards.py
@@ -1,17 +1,24 @@
 from random import randint, uniform
 from typing import Text, List, Tuple, Union
 
+from numpy import array
+from nptyping import Int8, NDArray, Shape
+
+
+IntArray = NDArray[Shape['*'], Int8]
+
 
 class Card:
-    # Aces have a default value of 11.
-    rank_map = {'A': 11, 'K': 10, 'Q': 10, 'J': 10, '10': 10, '9': 9,
-                '8': 8, '7': 7, '6': 6, '5': 5, '4': 4, '3': 3, '2': 2}
+    rank_map = {'A': array([11, 1], dtype=Int8), 'K': Int8(10), 'Q': Int8(10),
+                'J': Int8(10), '10': Int8(10), '9': Int8(9), '8': Int8(8), '7': Int8(7),
+                '6': Int8(6), '5': Int8(5), '4': Int8(4), '3': Int8(3), '2': Int8(2)}
 
     def __init__(self, rank: str, suit: Text, face_up=False):
         self.rank = rank
         self.suit = suit
         self.face_up = face_up
-        self.value: int = self.rank_map[rank]
+        self.value: Union[IntArray, Int8] = self.rank_map[rank]
+        self.ace = rank == 'A'
 
     def show(self) -> Tuple[str, str]:
         if self.face_up:
@@ -23,19 +30,24 @@ class Hand:
 
     def __init__(self, *args: Card):
         self.cards: List[Card] = list(args)
-        self.value: int = self._sum_values(*args)
+        self.value: Union[IntArray, Int8] = Int8(0)
+        self._add_values(*args)
 
     def add(self, *args: Card) -> None:
         self.cards.extend(args)
-        self.value += self._sum_values(*args)
+        self._add_values(*args)
         return None
 
     def show(self) -> List[Tuple[str, str]]:
         return [card.show() for card in self.cards]
 
-    @staticmethod
-    def _sum_values(*args: Card) -> Union[List[int], int]:
-        return sum(card.value for card in args)
+    def _add_values(self, *args: Card) -> None:
+        for card in args:
+            if card.ace and isinstance(self.value, IntArray):
+                self.value += 1
+            else:
+                self.value += card.value
+        return None
 
 
 class Deck:
