30668c125047dba56f320a89ba2f2883a36300e7
Zachary Chiodini <zacharybchiodini@gmail.com>
2022-06-20 00:11:25 -0400

    classes for cards

--- cards.py
+++ cards.py
@@ -3,7 +3,8 @@ from typing import Text, List, Tuple
 
 
 class Card:
-    rank_map = {'A': 1, 'K': 10, 'Q': 10, 'J': 10, '10': 10, '9': 9,
+    # Aces have a default value of 11.
+    rank_map = {'A': 11, 'K': 10, 'Q': 10, 'J': 10, '10': 10, '9': 9,
                 '8': 8, '7': 7, '6': 6, '5': 5, '4': 4, '3': 3, '2': 2}
 
     def __init__(self, rank: str, suit: Text, face_up=False):
