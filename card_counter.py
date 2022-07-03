ca54eac9dc9c6dcc8d14ea01ca5cf734b6f79f3a
Zachary Chiodini <zacharybchiodini@gmail.com>
2022-07-03 16:35:08 -0400

    Thinking about this guy ...

--- card_counter.py
+++ card_counter.py
@@ -1,7 +1,18 @@
 from basic_strategy import BasicStrategy
+from blackjack import Hand
 
 
 class CardCounter(BasicStrategy):
 
     def __init__(self):
         super().__init__()
+        self.name = 'Card Counter'
+
+    def ask_for_insurance(self) -> None:
+        pass
+
+    def call(self, hand: Hand) -> str:
+        pass
+
+    def place_bet(self, minimum_bet: int) -> bool:
+        pass
