86078ed6f8171c3ba0d6c790bad7a74a55a1caa0
Zachary Chiodini <zacharybchiodini@gmail.com>
2022-07-11 21:45:51 -0400

    how to keep card counter alive

--- simulation.py
+++ simulation.py
@@ -4,10 +4,14 @@ from card_counter import CardCounter
 if __name__ == '__main__':
     # Let's see if the card counter dies.
     card_counter = CardCounter()
+    card_counter.chips = 1000
     table = Table(players=1, decks=6, minimum_bet=50, penetration=0.75)
     table.players = [card_counter]
-    table.play(condition=lambda: card_counter.rounds < 1000)
-    if card_counter.rounds < 1000:
-        print(f'Card Counter DIED with {card_counter.chips} chips after {card_counter.rounds} rounds.')
+    n_games = 1000
+    table.play(condition=lambda: card_counter.rounds < n_games)
+    if card_counter.rounds < n_games:
+        print(f'Card Counter DIED with {card_counter.chips} chips after {card_counter.rounds} rounds '
+              f'and {round(card_counter.rounds / 50, 2)} hours.')
     else:
-        print(f'Card Counter SURVIVED with {card_counter.chips} after {card_counter.rounds} chips.')
+        print(f'Card Counter SURVIVED with {card_counter.chips} after {card_counter.rounds} rounds '
+              f'and {round(card_counter.rounds / 50, 2)} hours.')
