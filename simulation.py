3075f1a2ed2b62b258bf7b8029eb0546b219ab89
Zachary Chiodini <zacharybchiodini@gmail.com>
2022-07-12 18:53:00 -0400

    I am going to use ML to see if there is a way to optimize a 1000 dollar bankroll playing for 20 hours

--- simulation.py
+++ simulation.py
@@ -4,10 +4,10 @@ from card_counter import CardCounter
 if __name__ == '__main__':
     # Let's see if the card counter dies.
     card_counter = CardCounter()
-    card_counter.chips = 10000
-    table = Table(players=1, decks=6, minimum_bet=10, penetration=0.75)
+    card_counter.chips = 1000
+    table = Table(players=1, decks=6, minimum_bet=25, penetration=0.75)
     table.players = [card_counter]
-    n_games = 20000
+    n_games = 1000
     table.play(condition=lambda: card_counter.rounds < n_games)
     if card_counter.rounds < n_games:
         print(f'Card Counter DIED with {card_counter.chips} chips after {card_counter.rounds} rounds '
